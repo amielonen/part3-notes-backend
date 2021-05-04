e4f6e7d9cbe882edb350cdc48d9ea197f0ced270
amielonen <81672251+amielonen@users.noreply.github.com>
2021-05-04 08:15:35 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json())
 
@@ -79,7 +81,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
